6e3c9c514599ad33b11eaaa90bf04c1c6b317204
jagdep-singh <jagdeep.singh12a@gmail.com>
2026-07-12 13:41:20 +0530

    added friends frontend support

---
+++ components/expenses/friends/index.ts
@@ -0,0 +1,6 @@
+export * from "./friend-utils";
+export * from "./FriendCreateDialog";
+export * from "./FriendsSection";
+export * from "./FriendExpenseFields";
+export * from "./FriendSummaryLine";
+export * from "./use-friends-directory";
